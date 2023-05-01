d13b81cb5a6c2e3f2c3d4ad91582c5189a1fe8fd
pranshu-taneja <pranshu45taneja@gmail.com>
2023-05-01 20:54:59 +0530

    Added the jobList Background

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,7 @@ export default {
       colors:{
         prjblue: '#1597E4',
         FontColor: '#212121',
+        bgJobList: '#D8D8D8',
         pholder: '#7A7A7A',
         Error: '#D86161',
         CardBorder: '#E6E6E6',
