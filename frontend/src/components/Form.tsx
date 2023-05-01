e2d6825339147a60f6bfaecb354fad8a9ed0ccce
pranshu-taneja <pranshu45taneja@gmail.com>
2023-05-01 20:59:57 +0530

    Cleaned Code, Alerts, Removed Unused elements

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -4,27 +4,31 @@ import { useState, FormEvent } from "react";
 import useMultistepForm from "./useMultistepForm";
 import axios from "axios";
 
+
 type FormData = {
+  //step-1 field data
   JobTitle: string;
   CompanyName: string;
   Industry: string;
   Location: string;
   RemoteType: string;
 
-  // step 2 data
+  //step-2 field data
   Experience: {
-    min: number;
-    max: number;
+    min: string;
+    max: string;
   };
   Salary: {
-    min: number;
-    max: number;
+    min: string;
+    max: string;
   };
-  TotalEmployee: number;
+  TotalEmployee: string;
   ApplyType: string;
 };
 
+//Dummy data for Intial state
 const Intial_Data: FormData = {
+  //step 1 data
   JobTitle: "",
   CompanyName: "",
   Industry: "",
@@ -33,35 +37,42 @@ const Intial_Data: FormData = {
 
   // step 2 data
   Experience: {
-    min: 0,
-    max: 0,
+    min: "",
+    max: "",
   },
   Salary: {
-    min: 0,
-    max: 0,
+    min: "",
+    max: "",
   },
-  TotalEmployee: 0,
+  TotalEmployee: "",
   ApplyType: "",
 };
 
 export default function Form() {
   const [data, setdata] = useState(Intial_Data);
 
+  //------------------- Functionality of how data will be updated with state -------------------
   function UpdateFields(fields: Partial<FormData>) {
     setdata((prev) => {
       return { ...prev, ...fields };
     });
   }
+  //------------------- Functionality of how data will be updated with state -------------------
 
+  //------------------- Communication with the custom Prop -------------------
   const {
-    NofSteps,
     currentStepIndex,
-    isFirstStep,
     isLastStep,
     step,
-    prev,
     next,
-  } = useMultistepForm([<CreateJob1 {...data} UpdateFields={UpdateFields} />, <CreateJob2 {...data} UpdateFields={UpdateFields}/>]);
+  } = useMultistepForm([
+    //------------------- Passing the data & Method for updating state -------------------
+    <CreateJob1 {...data} UpdateFields={UpdateFields} />,
+    <CreateJob2 {...data} UpdateFields={UpdateFields} />,
+    //------------------- Passing the data & Method for updating state -------------------
+  ]);
+  //------------------- Communication with the custom Prop -------------------
+
 
   //------------------- Posting data using axios -------------------
   async function onSubmit(e: FormEvent) {
@@ -74,8 +85,9 @@ export default function Form() {
         "https://6446405fee791e1e29fa0001.mockapi.io/card-detail/",
         data
       );
+      alert("Job Created Successfully");
     } catch (err) {
-      console.log({ Error: "Some Error Occured" });
+      alert("Some Error Occured");
       console.log(err);
     }
   }
@@ -83,43 +95,30 @@ export default function Form() {
 
   return (
     <div className="top-32 relative w-[577px] h-[564px] bg-white flex flex-col rounded-[10px] border-CardBorder p-8 border">
-      {/* <div className="flex h-[100vh] justify-center"> */}
       <form onSubmit={onSubmit}>
         <div className="flex justify-between flex-row mb-[24px]">
           <p className="font-poppins font-[400] text-[20px]/[28px]">
             Create a job
           </p>
           <p className="relative font-poppins font-[500] text-[16px]/[24px] top-[2px]">
-            Step {currentStepIndex + 1} of {NofSteps}
+            Step {currentStepIndex + 1}
           </p>
         </div>
 
-        {/* Fields */}
+        {/* Input Fields */}
         {step}
-        {/* Fields */}
+        {/* Input Fields */}
 
-        {/* //------------------- Buttons ------------------- */}
+        {/* //------------------- Next Button ------------------- */}
         <div className="flex justify-end mt-[86px] font-poppins">
-          {!isFirstStep && (
-            <button
-              className="rounded-[6px] mr-5 py-[8px] px-[16px] bg-prjblue text-[16px]/[24px] font-poppins font-[500] text-white"
-              type="submit"
-              onClick={prev}
-            >
-              Back
-            </button>
-          )}
           <button
             className="rounded-[6px] py-[8px] px-[16px] bg-prjblue text-[16px]/[24px] font-poppins font-[500] text-white"
             type="submit"
           >
-            {!isLastStep ? "Next" : "Submit"}
+            {!isLastStep ? "Next" : "Save"}
           </button>
         </div>
-        {/* //------------------- Buttons ------------------- */}
-
-
-
+        {/* //------------------- Next Button ------------------- */}
       </form>
     </div>
     // </div>
